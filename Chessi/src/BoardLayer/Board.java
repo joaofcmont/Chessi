947636bc1506fc19c627075bf5717ba391932530
joaomonteiro <jfcmo98647@gmail.com>
2024-08-28 16:59:21 +0100

    Add methods to check if there is a piece in place

--- Chessi/src/BoardLayer/Board.java
+++ Chessi/src/BoardLayer/Board.java
@@ -11,6 +11,9 @@ public class Board {
 
 
     public Board(int rows, int columns) {
+        if(rows<1 || columns<1){
+            throw new BoardException("Error creating board: There must be at least 1 row and 1 column");
+        }
         this.rows = rows;
         this.columns = columns;
         pieceList = new Piece[rows][columns];
@@ -25,15 +28,42 @@ public class Board {
     }
 
     public Piece piece(int row, int columns){
+        if(!positionExists(row,columns)){
+            throw new BoardException("Position not on the Board");
+        }
         return pieceList[row][columns];
     }
 
     public Piece piece(Position position){
+        if(!positionExists(position)){
+            throw new BoardException("Position not on the Board");
+        }
         return pieceList[position.getRow()][position.getColumn()];
     }
+
     public void placePiece(Piece piece, Position position){
+        if(thereIsAPiece(position)){
+            throw new BoardException("There is already a piece on the position " + position);
+        }
         pieceList[position.getRow()][position.getColumn()] = piece;
         piece.position = position;
     }
+
+    private boolean positionExists(int row, int column){
+        return row >= 0 && row < rows && column >=0 && column >columns;
+    }
+
+    public boolean positionExists(Position position){
+        return positionExists(position.getRow(), position.getColumn());
+    }
+
+    public boolean thereIsAPiece(Position position){
+        if(!positionExists(position)){
+            throw new BoardException("Position not on the board");
+        }
+        return piece(position) != null;
+    }
+
+
 
 }
