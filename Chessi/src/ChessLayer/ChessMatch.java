abd1b6b0603b3522a3465929f53ec4afc538555b
joaomonteiro <jfcmo98647@gmail.com>
2024-08-28 18:04:06 +0100

    Adding all Pawns to the initial setup

--- Chessi/src/ChessLayer/ChessMatch.java
+++ Chessi/src/ChessLayer/ChessMatch.java
@@ -3,6 +3,7 @@ package ChessLayer;
 import BoardLayer.Board;
 import BoardLayer.Position;
 import ChessLayer.pieces.King;
+import ChessLayer.pieces.Pawn;
 import ChessLayer.pieces.Rook;
 
 public class ChessMatch {
@@ -30,8 +31,24 @@ public class ChessMatch {
 
     public void initialSetup(){
         placeNewPiece('b', 6 ,new Rook(board,Color.WHITE));
-        placeNewPiece('e', 8 ,new King(board,Color.BLACK));
-        placeNewPiece('e', 1 ,new King(board,Color.WHITE));
+        placeNewPiece('a', 2 ,new Pawn(board,Color.WHITE));
+        placeNewPiece('b', 2 ,new Pawn(board,Color.WHITE));
+        placeNewPiece('c', 2 ,new Pawn(board,Color.WHITE));
+        placeNewPiece('d', 2 ,new Pawn(board,Color.WHITE));
+        placeNewPiece('e', 2 ,new Pawn(board,Color.WHITE));
+        placeNewPiece('f', 2 ,new Pawn(board,Color.WHITE));
+        placeNewPiece('g', 2 ,new Pawn(board,Color.WHITE));
+        placeNewPiece('h', 2 ,new Pawn(board,Color.WHITE));
+
+        placeNewPiece('a', 7 ,new Pawn(board,Color.BLACK));
+        placeNewPiece('b', 7 ,new Pawn(board,Color.BLACK));
+        placeNewPiece('c', 7 ,new Pawn(board,Color.BLACK));
+        placeNewPiece('d', 7 ,new Pawn(board,Color.BLACK));
+        placeNewPiece('e', 7 ,new Pawn(board,Color.BLACK));
+        placeNewPiece('f', 7 ,new Pawn(board,Color.BLACK));
+        placeNewPiece('g', 7 ,new Pawn(board,Color.BLACK));
+        placeNewPiece('h', 7 ,new Pawn(board,Color.BLACK));
+
 
     }
 }
