415701eb65cbfc33bac309d88051602ef28e066d
joaomonteiro <jfcmo98647@gmail.com>
2024-08-30 16:46:50 +0100

    Adding color

--- Chessi/src/program/Main.java
+++ Chessi/src/program/Main.java
@@ -1,12 +1,29 @@
 package program;
 
 import ChessLayer.ChessMatch;
+import ChessLayer.ChessPiece;
+import ChessLayer.ChessPosition;
+
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
 
         ChessMatch chessMatch = new ChessMatch();
-        UI.printBoard(chessMatch.getPieces());
+        Scanner sc = new Scanner(System.in);
 
+        while(true) {
+            UI.printBoard(chessMatch.getPieces());
+            System.out.println();
+            System.out.print("Source: ");
+            ChessPosition source = UI.readChessPosition(sc);
+
+            System.out.println();
+            System.out.print("Target: ");
+            ChessPosition target = UI.readChessPosition(sc);
+
+            ChessPiece capturedPiece = chessMatch.performChessMove(source,target);
+
+        }
     }
 }
