0a03326f7709c893afb227b7430baa3a52dcda58
joaomonteiro <jfcmo98647@gmail.com>
2024-08-30 15:57:00 +0100

    Adding color to the pieces

--- Chessi/src/program/Main.java
+++ Chessi/src/program/Main.java
@@ -5,7 +5,6 @@ import ChessLayer.ChessMatch;
 public class Main {
     public static void main(String[] args) {
 
-
         ChessMatch chessMatch = new ChessMatch();
         UI.printBoard(chessMatch.getPieces());
 
