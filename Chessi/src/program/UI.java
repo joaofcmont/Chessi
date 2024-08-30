196c6a0a95693e7f374cefde03c8712094c1e4b9
joaomonteiro <jfcmo98647@gmail.com>
2024-08-30 16:08:17 +0100

    Adding color to the pieces

--- Chessi/src/program/UI.java
+++ Chessi/src/program/UI.java
@@ -27,28 +27,29 @@ public class UI {
     public static final String ANSI_WHITE_BACKGROUND = "\u001B[47m";
 
 
-    public static void printBoard(ChessPiece[][] pieces, boolean [][] possibleMoves){
+    public static void printBoard(ChessPiece[][] pieces){
         for(int i=0;i<pieces.length;i++){
             System.out.print((8-i) + " ");
             for(int j=0;j<pieces.length;j++){
-                printPiece(pieces[i][j], possibleMoves[i][j]);
+                printPiece(pieces[i][j]);
             }
             System.out.println();
         }
         System.out.println("  a b c d e f g h");
     }
 
-    private static void printPiece(ChessPiece piece, boolean background){
-        if (background) {
-            System.out.print(ANSI_BLUE_BACKGROUND);
-        }else {
+    private static void printPiece(ChessPiece piece){
+        if (piece == null) {
+            System.out.print("-" + ANSI_RESET);
+        }
+            else {
             if (piece.getColor() == Color.WHITE) {
                 System.out.print(ANSI_WHITE + piece + ANSI_RESET);
-            }
-            else {
+            } else {
                 System.out.print(ANSI_YELLOW + piece + ANSI_RESET);
             }
         }
+
         System.out.print(" ");
     }
 
