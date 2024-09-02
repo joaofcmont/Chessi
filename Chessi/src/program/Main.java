b473d864e112ec9147c6f67e6ebcdb9c8161ffd4
joaomonteiro <jfcmo98647@gmail.com>
2024-09-02 18:03:40 +0100

    Updating try-catch block

--- Chessi/src/program/Main.java
+++ Chessi/src/program/Main.java
@@ -9,24 +9,26 @@ import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) throws ChessException, InputMismatchException{
+    public static void main(String[] args) {
 
         ChessMatch chessMatch = new ChessMatch();
         Scanner sc = new Scanner(System.in);
 
         while (true) {
+            try {
+                UI.clearScreen();
+                UI.printBoard(chessMatch.getPieces());
+                System.out.println();
+                System.out.print("Source: ");
+                ChessPosition source = UI.readChessPosition(sc);
+                System.out.println();
+                System.out.print("Target: ");
+                ChessPosition target = UI.readChessPosition(sc);
 
-            UI.clearScreen();
-            UI.printBoard(chessMatch.getPieces());
-            System.out.println();
-            System.out.print("Source: ");
-            ChessPosition source = UI.readChessPosition(sc);
-            System.out.println();
-            System.out.print("Target: ");
-            ChessPosition target = UI.readChessPosition(sc);
-
-            ChessPiece capturedPiece = chessMatch.performChessMove(source,target);
-
+                ChessPiece capturedPiece = chessMatch.performChessMove(source, target);
+            }catch (ChessException | InputMismatchException e){
+                System.out.println(e.getMessage());
+            }
         }
     }
 }
