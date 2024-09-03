559c256e77d9cd33f1a1e88c6682fce2627cebb1
joaomonteiro <jfcmo98647@gmail.com>
2024-09-03 18:17:13 +0100

    Adding King movement

--- Chessi/src/ChessLayer/pieces/King.java
+++ Chessi/src/ChessLayer/pieces/King.java
@@ -1,6 +1,7 @@
 package ChessLayer.pieces;
 
 import BoardLayer.Board;
+import BoardLayer.Position;
 import ChessLayer.ChessPiece;
 import ChessLayer.Color;
 
@@ -17,6 +18,66 @@ public class King extends ChessPiece {
     @Override
     public boolean[][] PossibleMoves() {
         boolean[][] mat = new boolean[getBoard().getRows()][getBoard().getColumns()];
+
+        Position p = new Position(position.getRow(),position.getColumn());
+
+        //check above
+        p.setValues(position.getRow()-1,position.getColumn());
+        if(getBoard().positionExists(p) && (!getBoard().thereIsAPiece(p)||isThereOpponentPiece(p) )){
+            mat[p.getRow()][p.getColumn()] = true;
+            p.setRow(p.getRow()-1);
+        }
+
+        //check down
+        p.setValues(position.getRow()+1,position.getColumn());
+        if(getBoard().positionExists(p) && (!getBoard().thereIsAPiece(p)||isThereOpponentPiece(p) )){
+            mat[p.getRow()][p.getColumn()] = true;
+            p.setRow(p.getRow()+1);
+        }
+        //check left
+        p.setValues(position.getRow(),position.getColumn()-1);
+        if(getBoard().positionExists(p) && (!getBoard().thereIsAPiece(p)||isThereOpponentPiece(p) )){
+            mat[p.getRow()][p.getColumn()] = true;
+            p.setColumn(p.getColumn()-1);
+        }
+
+        //check right
+        p.setValues(position.getRow(),position.getColumn()+1);
+        if(getBoard().positionExists(p) && (!getBoard().thereIsAPiece(p)||isThereOpponentPiece(p) )){
+            mat[p.getRow()][p.getColumn()] = true;
+            p.setColumn(p.getColumn()+1);
+        }
+
+        //check diagonal top-left
+        p.setValues(position.getRow()-1,position.getColumn()-1);
+        if(getBoard().positionExists(p) && (!getBoard().thereIsAPiece(p)||isThereOpponentPiece(p) )){
+            mat[p.getRow()][p.getColumn()] = true;
+            p.setValues(p.getRow()-1, p.getColumn()-1);
+        }
+
+        //diagonal top-right
+        p.setValues(position.getRow()-1,position.getColumn()+1);
+        if(getBoard().positionExists(p) && (!getBoard().thereIsAPiece(p)||isThereOpponentPiece(p) )){
+            mat[p.getRow()][p.getColumn()] = true;
+            p.setValues(p.getRow()-1, p.getColumn()+1);
+        }
+
+
+        //check diagonal down-right
+        p.setValues(position.getRow()+1,position.getColumn()+1);
+        if(getBoard().positionExists(p) && (!getBoard().thereIsAPiece(p)||isThereOpponentPiece(p) )){
+            mat[p.getRow()][p.getColumn()] = true;
+            p.setValues(p.getRow()+1, p.getColumn()+1);
+        }
+
+        //check diagonal down-left
+        p.setValues(position.getRow()+1,position.getColumn()-1);
+        if(getBoard().positionExists(p) && (!getBoard().thereIsAPiece(p)||isThereOpponentPiece(p) )){
+            mat[p.getRow()][p.getColumn()] = true;
+            p.setValues(p.getRow()+1, p.getColumn()-1);
+        }
+
+
         return mat;
     }
 }
