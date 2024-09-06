befe32a330bb063c480d12e4f31e5b2c9fccdba8
joaomonteiro <jfcmo98647@gmail.com>
2024-09-06 11:19:55 +0100

    Adding a GUI Visual

--- Chessi/src/program/Main.java
+++ Chessi/src/program/Main.java
@@ -5,6 +5,7 @@ import ChessLayer.ChessMatch;
 import ChessLayer.ChessPiece;
 import ChessLayer.ChessPosition;
 
+import javax.swing.*;
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
@@ -12,30 +13,9 @@ public class Main {
     public static void main(String[] args) {
 
         ChessMatch chessMatch = new ChessMatch();
-        Scanner sc = new Scanner(System.in);
 
-        while (true) {
-            try {
-                UI.clearScreen();
-                UI.printBoard(chessMatch.getPieces());
-                System.out.println();
-                System.out.print("Source: ");
-                ChessPosition source = UI.readChessPosition(sc);
-
-                boolean[][] possiblePosition = chessMatch.possibleMoves(source);
-                UI.clearScreen();
-                UI.printBoard(chessMatch.getPieces(), possiblePosition);
-
-                System.out.println();
-                System.out.print("Target: ");
-                ChessPosition target = UI.readChessPosition(sc);
-
-                ChessPiece capturedPiece = chessMatch.performChessMove(source, target);
-            }catch (ChessException | InputMismatchException e) {
-                System.out.println(e.getMessage());
-                sc.nextLine();
-            }
-        }
+        LauchPage launchPage = new LauchPage();
+        SwingUtilities.invokeLater(LauchPage::new);
 
     }
 
